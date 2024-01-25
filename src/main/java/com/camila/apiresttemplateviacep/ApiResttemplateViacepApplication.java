e8a6af22698c362a9885965539ed2830a0486711
Camila <alvescamila87@gmail.com>
2024-01-25 19:49:34 -0300

    generate files from spring.inicializer

---
+++ src/main/java/com/camila/apiresttemplateviacep/ApiResttemplateViacepApplication.java
@@ -0,0 +1,13 @@
+package com.camila.apiresttemplateviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResttemplateViacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResttemplateViacepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camila/apiresttemplateviacep/ApiResttemplateViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camila.apiresttemplateviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResttemplateViacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
